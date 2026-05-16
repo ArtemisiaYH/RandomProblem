b74f2f5011e87a43a7ca06cd7bdd00848908a1a9
ArtemisiaYH <yomoplay@gmail.com>
2026-05-16 17:36:53 +0900

    うわがき

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import json
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-sid = int(os.getenv("SERVER_ID"))
 PREFIX = '!'
 
 intents = discord.Intents.default()
@@ -20,11 +19,7 @@ bot = commands.Bot(command_prefix=PREFIX,intents=intents)
 async def on_ready():
     await bot.load_extension("cogs.problem")
     await bot.tree.sync()
-    await bot.tree.sync(guild=discord.Object(id=sid))
     print(f'Logged in as {bot.user.name}')
     print(f'登録済みコマンド: {[c.name for c in bot.tree.get_commands()]}')
-async def on_interaction(interaction: discord.Interaction):
-    print(f"インタラクション受信: {interaction.type} {interaction.data}")
-    await bot.process_application_commands(interaction)
 
 bot.run(TOKEN)
