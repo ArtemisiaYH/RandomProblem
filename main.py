eda1673292415c52171ee9585666e9daa44e6dce
ArtemisiaYH <yomoplay@gmail.com>
2026-05-17 14:49:21 +0900

    done

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ bot = commands.Bot(command_prefix=PREFIX,intents=intents)
 
 @bot.event
 async def on_ready():
-    await bot.load_extension("cogs.problem")
-    await bot.load_extension("cogs.user")
+    await bot.load_extension("cogs.problem") # /problem
+    await bot.load_extension("cogs.user") # /user
     await bot.tree.sync()
     print(f'Logged in as {bot.user.name}')
     print(f'登録済みコマンド: {[c.name for c in bot.tree.get_commands()]}')
